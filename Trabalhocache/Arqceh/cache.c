f6aa09ebf6352e5180b6bc28ded866b9eb9fe1b0
João Vítor Maciel de Brito <54175322+joao369@users.noreply.github.com>
2022-09-22 20:56:39 -0300

    Update cache.c

--- Trabalhocache/Arqceh/cache.c
+++ Trabalhocache/Arqceh/cache.c
@@ -2,30 +2,47 @@
 #include <stdlib.h>
 #include "cache.h"
 
-struct cache* criar_cache(unsigned int tamanho, unsigned int Tam_bloco){
-	unsigned int Num_set = tamanho/(4*Tam_bloco), i=0, j=0;
+struct cache {
+	unsigned int tam;
+	unsigned int tBloco;
+	unsigned int num_linhas;
+	struct bloco_cache* bloco;
+};
+struct bloco_cache {
+	unsigned int dados;
+	unsigned int tag;
+	unsigned int valido;
+};
+
+struct cache* criar_cache(const unsigned int tam, const unsigned int tam_bloco){
+	unsigned int num_linhas = tam/(tam_bloco*4);
+	int i;
 	struct cache* c = malloc(sizeof(struct cache));
-	c->bloco = (struct Bloco_cache**)malloc(Num_set*sizeof(struct Bloco_cache*));
-	for(i=0;i<Num_set;i++){
-		c->bloco[i] = (struct Bloco_cache*)malloc(4*sizeof(struct Bloco_cache));
-		for(j=0;j<4;j++){
-			c->bloco[i][j].valido = 0;
-			c->bloco[i][j].indice = 0;
-			c->bloco[i][j].tag = 0;
-		}
+	c->bloco = (struct bloco_cache*)malloc((num_linhas*4)*sizeof(struct bloco_cache));
+	for(i=0;i<(num_linhas*4);i++){
+		c->bloco[i].tag = 0;
+		c->bloco[i].valido = 0;
+		c->bloco[i].dados = 0;
 	}
-	c->tam = tamanho;
-	c->Tbloco = Tam_bloco;
-	c->Num_set = Num_set;
+	c->tam = tam;
+	c->tBloco = tam_bloco;
+	c->num_linhas = num_linhas;
 	
+	return c;
 }
 
-
-void freeCache(Struct cache* c){
-	unsigned int i;
-	for(i=0;i<c->Num_set;i++){
-		free(c->bloco[i]);
-	}
+void freeCache(struct cache* c){
 	free(c->bloco);
-	free(c);
+}
+
+Procurar_cache(struct cache* c, int num){
+	int i, indice=0;
+	for(i=0;i<c->num_linhas;i++){
+		if(num & 1<<i){
+			indice++;
+		}
+	}
+	for(i=0;i<c->num_linhas;i+=4){
+		
+	}
 }
