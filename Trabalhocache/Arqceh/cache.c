86b30eb7416b16921441f5bb57a3eacad20a3c27
João Vítor Maciel de Brito <54175322+joao369@users.noreply.github.com>
2022-09-26 12:32:49 -0300

    Update cache.c

--- Trabalhocache/Arqceh/cache.c
+++ Trabalhocache/Arqceh/cache.c
@@ -2,47 +2,100 @@
 #include <stdlib.h>
 #include "cache.h"
 
-struct cache {
-	unsigned int tam;
-	unsigned int tBloco;
-	unsigned int num_linhas;
-	struct bloco_cache* bloco;
-};
-struct bloco_cache {
-	unsigned int dados;
-	unsigned int tag;
-	unsigned int valido;
-};
 
-struct cache* criar_cache(const unsigned int tam, const unsigned int tam_bloco){
-	unsigned int num_linhas = tam/(tam_bloco*4);
-	int i;
+
+struct cache* criar_cache(unsigned int tamanho, unsigned int Tam_bloco,unsigned int tc, unsigned int TRM, unsigned int TWM){
+	unsigned int indice = tamanho/(4*Tam_bloco), i, j;
 	struct cache* c = malloc(sizeof(struct cache));
-	c->bloco = (struct bloco_cache*)malloc((num_linhas*4)*sizeof(struct bloco_cache));
-	for(i=0;i<(num_linhas*4);i++){
-		c->bloco[i].tag = 0;
-		c->bloco[i].valido = 0;
-		c->bloco[i].dados = 0;
+	c->bloco = (struct Bloco_cache**)malloc(indice*sizeof(struct Bloco_cache*));
+	for(i=0;i<indice;i++){
+		c->bloco[i] = malloc(4*sizeof(struct Bloco_cache));
+		for(j=0;j<4;j++){
+			c->bloco[i][j].dados = 0;
+			c->bloco[i][j].tag = 0;
+			c->bloco[i][j].valido = 0;
+		}
 	}
-	c->tam = tam;
-	c->tBloco = tam_bloco;
-	c->num_linhas = num_linhas;
+	c->Tam = tamanho;
+	c->Tbloco = Tam_bloco;
+	c->num_linhas = indice;
+	c->hit = 0;
+	c->miss = 0;
+	c->tc = tc;
+	c->tr = TRM;
+	c->tw = TWM;
+	c->tt = 2*c->tw;
 	
 	return c;
+	
 }
 
 void freeCache(struct cache* c){
+	int i;
+	for(i=0;i<c->num_linhas;i++){
+		free(c->bloco[i]);
+	}
 	free(c->bloco);
+	free(c);
 }
 
-Procurar_cache(struct cache* c, int num){
-	int i, indice=0;
-	for(i=0;i<c->num_linhas;i++){
-		if(num & 1<<i){
-			indice++;
+struct cache* Procurar_cache(struct cache* c,unsigned int ciclo, char tipo,unsigned int end){
+	unsigned int i;
+	unsigned int num_bit_tag = 64-(2 + c->num_linhas);
+	unsigned int tag_end = (end <<(2 + c->num_linhas));
+	unsigned int indice_end = (num_bit_tag >> end <<  2);
+	
+	for(i=0;i<4;i++){
+		if(c->bloco[indice_end][i].tag == tag_end){
+			if(tipo == 'W'){
+				c->tt += c->tc;
+			}
+			else {
+				c->tt += c->tc + c->tr;
+			}
+			c->hit++;
+			LRU(c,tag_end, indice_end);
+			break;
+		}
+		else if(i==3){
+			c->miss++;
+			if(tipo == 'R'){
+				c->tt += c->tc + c->tr;
+				escreve_cache(c,tag_end,end,indice_end);
+				LRU(c, tag_end, indice_end);
+			}
+			else {
+				c->tt += c->tc + c->tw;
+			}
+			
 		}
 	}
-	for(i=0;i<c->num_linhas;i+=4){
-		
+	
+	
+	
+	return c;
+	
+}
+
+void LRU(struct cache* c, unsigned int tag, unsigned int indice){
+	unsigned int i;
+	struct Bloco_cache* bloco_aux;
+	bloco_aux = c->bloco[indice];
+	
+	for(i=indice;i>0;i--){
+		c->bloco[i] = c->bloco[i-1];
+	}
+	c->bloco[0] = bloco_aux;
+}
+
+void escreve_cache(struct cache* c, unsigned int tag, unsigned int dado, unsigned int indice){
+	int i;
+	for(i=0;i<4;i++){
+		if(c->bloco[indice][i].valido == 0){
+			c->bloco[indice][i].tag = tag;
+			c->bloco[indice][i].dados = dado;
+			c->bloco[indice][i].valido = 1;
+			break;
+		}
 	}
 }
