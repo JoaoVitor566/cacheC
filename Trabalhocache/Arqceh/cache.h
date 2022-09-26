00b6239fdca10fd4bde88933317d0d15762811e0
João Vítor Maciel de Brito <54175322+joao369@users.noreply.github.com>
2022-09-26 12:32:11 -0300

    Update cache.h

--- Trabalhocache/Arqceh/cache.h
+++ Trabalhocache/Arqceh/cache.h
@@ -1,15 +1,37 @@
-struct cache {
-	unsigned int tam;
-	unsigned int Num_set;
-	unsigned int Tbloco;
-	struct Bloco_cache bloco;
-	
-};
+#ifndef CACHE_H
+#define CACHE_H
+#include <stdlib.h>
 
-struct Bloco_cache {
-	bool indice;
-	bool valido;
-	unsigned int tag;
-	unsigned int dado;
+struct cache {
+	unsigned int Tam;
+	unsigned int Tbloco;
+	struct Bloco_cache** bloco;
+	unsigned int num_linhas;
+	
+	unsigned int hit;
+	unsigned int miss;
+	
+	unsigned int tc;
+	unsigned int tw;
+	unsigned int tr;
+	unsigned int tt;
+	
+		
 };
+	
+struct Bloco_cache {
+	unsigned int valido;
+	unsigned int tag;
+	unsigned int dados;
+};
+	
+struct cache* criar_cache(unsigned int Tam, unsigned int Bcache,unsigned int tc, unsigned int TRM, unsigned int TWM);
+void freeCache(struct cache* c);
+struct cache* Procurar_cache(struct cache* c,unsigned int ciclo, char tipo, unsigned int end);
+void LRU(struct cache* c, unsigned int tag, unsigned int indice);
+void escreve_cache(struct cache* c, unsigned int tag, unsigned int dado, unsigned int indice);
+	
+	
+	
+#endif
 
